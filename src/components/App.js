2d7afc7761132650c2f6e218967e23b77e98531d
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-04-21 12:55:07 -0500

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -96,6 +96,7 @@ export default function App() {
       // 🔥 STEP 7- WHAT ABOUT HOBBIES?
     };
     // 🔥 STEP 8- POST NEW FRIEND USING HELPER
+    postNewFriend(newFriend);
   };
 
   //////////////// SIDE EFFECTS ////////////////
