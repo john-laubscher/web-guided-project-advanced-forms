4f98266517809ec7d2226efd5f7368a65bdf5091
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-04-21 12:41:19 -0500

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -6,22 +6,16 @@ export default yup.object().shape({
     .string()
     .required("username is required")
     .min(3, "username must be 3 chars long"),
-  email: yup.string(),
-  role: yup.string(),
-  civil: yup.string(),
+  email: yup
+    .string()
+    .email("must be a valid email")
+    .required("email is required"),
+  role: yup
+    .string()
+    .oneOf(["tl", "instructor", "alumni", "student"], "role is required"),
+  civil: yup.string().oneOf(["married", "single"], "civil status is required"),
   // we are done with checkboxes
   coding: yup.boolean(),
   hiking: yup.boolean(),
   reading: yup.boolean(),
-});
-
-const formSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Must be a valid email address.")
-    .required("Must include email address."),
-  password: Yup.string()
-    .required("Password is Required")
-    .min(6, "Passwords must be at least 6 characters long."),
-  terms: Yup.boolean().oneOf([true], "You must accept Terms and Conditions"),
-  // required isn't required for checkboxes.
 });
