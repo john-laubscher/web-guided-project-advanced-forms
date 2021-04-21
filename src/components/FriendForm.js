e0034fbecbd437c440c4a63a09fe8a99de3ce8f1
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-04-21 11:48:43 -0500

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -85,7 +85,16 @@ export default function FriendForm(props) {
           />
         </label>
 
-        <label>Married</label>
+        <label>
+          Married
+          <input
+            type="radio"
+            name="civil"
+            value="married"
+            checked={values.civil === "married"}
+            onChange={onChange}
+          />
+        </label>
       </div>
 
       <div className="form-group checkboxes">
